d6fe93bfe086b70be00676ccff2bf40f44c7a557
MizenkoEugen <MizenkoEugen@gmail.com>
2022-12-19 00:03:57 +0300

    add homework_02

--- Seminar_2/Task_04.py
+++ Seminar_2/Task_04.py
@@ -1,3 +1,28 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
-# Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+# Найдите произведение элементов на указанных позициях.
+# Позиции хранятся в файле file.txt в одной строке одно число.
 
+import random
+
+N = int(input('Введите число: '))
+numbers = []
+for i in range(N):
+    numbers.append(random.randint(-N,N))
+print(numbers)
+
+f = open('file.txt', 'w')
+while True:
+    s = input('Укажите индекс элемента: ')
+    if s == "" or int(s) >= N:
+        break
+    f.write(s+"\n")
+f.close()
+
+result = 1
+f = open('file.txt', 'r')
+for line in f:
+    if line == "":
+        break
+    result *= numbers[int(line)]
+f.close()
+print(f'Произведение указанных элементов: {result}')
--- Seminar_2/Task_05.py
+++ Seminar_2/Task_05.py
@@ -1,2 +1,10 @@
 # Реализуйте алгоритм перемешивания списка.
 
+import random
+
+lst = []
+for i in range(5):
+    lst.append(random.randint(0,5))
+print(f"исходный список:\n{lst}")
+random.shuffle(lst)
+print(f"список после перемешивания:\n{lst}")
